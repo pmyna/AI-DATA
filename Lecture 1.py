51ff87b0b58849cfa8b160aa3cffd9d419c35895
pmyna <miname630@gmail.com>
2022-04-07 10:36:21 +0200

    Notebook Files added

---
+++ Lecture 1.py
@@ -0,0 +1,16 @@
+# Schreibe eine rekursive Funktion fac_rec(num), die die Zahlen von 1 bis num multipliziert.
+
+def fac_rec(num):
+    pass
+
+
+# Schreibe eine rekursive Funktion fib_rec(n), die die nte Fibonacci-Zahl.berechnet.
+
+def fib_rec(n):
+    pass
+
+
+# Schreibe eine Funktion compound_interest(base_interest, run_time) die den Zinseszins berechnet
+
+def compound_interest(base_interest, run_time):
+    return 0
